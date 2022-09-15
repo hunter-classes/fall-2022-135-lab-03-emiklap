1a86c4cb9a4ed0708de9ad814ba494d2e0094127
Emily Klapper <emiklap@gmail.com>
2022-09-15 19:57:22 -0400

    initial commit (lab 3-a)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 // add your code as appropriate 
 #include <iostream>
+#include "reservoir.h"
 
 int main()
 {
-  
+  get_east_storage("01/01/2018");
 
   
   return 0;
--- reservoir.cpp
+++ reservoir.cpp
@@ -1,1 +1,28 @@
-// add your code
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <climits>
+
+double get_east_storage(std::string input_date) {
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+
+	if (fin.fail()) {
+		std::cerr << "FIle cannot be opened for readin." << std::endl;
+		exit(1);
+	}
+
+	std::string junk;
+	getline(fin, junk);
+
+	std::string date;
+	double eastSt, eastEl, westSt, westEl;
+
+	while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) {
+		fin.ignore(INT_MAX, '\n');
+		if (date == input_date) {
+			std::cout << date << " " << eastSt << std::endl;
+		}
+	}
+	fin.close();
+	return 0;
+}
